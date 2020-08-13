ed6b96d1be0a8b9ef11d2a3dfa25853642fff8e9
cleminator <alvinclemens@gmail.com>
2020-08-12 23:20:40 -0600

    long night

--- java.js
+++ java.js
@@ -4,19 +4,16 @@
 $(document).ready(function () {
 
     // I want to play with the idea of hiding my cards. with this i have hidden my 5th jumbotron 
-    $('div#fun2,#fun3,#fun4,#fun5,#endGame').hide();
+    $('div#fun2,#fun3,#fun4,#fun5,#fun6,#fun7,#fun8,#fun9,#fun10,#fun11,#endGame').hide();
     // this is a working alert tied to the start Btn
 
     let scoreup = 0
-    let scoredown = 0
+
 
     // these are my button functions to hide and show the questions
     // start button known as #tryme
     $('#tryme').on('click', function () {
-        $('#fun1').hide();
-    });
-    $('#tryme').on('click', function () {
-        $('#fun2').show();
+        $('#fun1').hide(); $('#fun2').show();
     });
 
     // first question button known as #winner 1
@@ -28,32 +25,31 @@ $(document).ready(function () {
         console.log(scoreup)
     });
     // losses will be move you to the next question
-    $("#looser1").on('click', function () {
-        scoredown = scoreup - 1
+    $("#looser1,#looser5").on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
+        scoreup = scoreup - 1
         $('#score').html(scoreup);
         $('#fun3').show(); $("#fun2").hide();
         console.log("#looser1")
     });
 
-    // looser buttons must be added for each
-
-
     // second question button known as #winner 2
 
     $('#winner2').on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
         scoreup = scoreup + 1
         $('#score').html(scoreup);
         $('#fun4').show(); $('#fun3').hide();
         console.log(scoreup)
     });
-    $("#looser2").on('click', function () {
+    $("#looser2,#looser6").on('click', function () {
         scoreup = scoreup - 1
         $('#score').html(scoreup);
         $('#fun4').show(); $("#fun3").hide();
-        console.log("#looser1")
+        console.log("Hlooser2")
     });
-
-
 
     // third question button known as #winner 3
 
@@ -63,7 +59,9 @@ $(document).ready(function () {
         $('#fun5').show(); $('#fun4').hide();
     });
 
-    $("#looser3").on('click', function () {
+    $("#looser3,#looser7").on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
         scoreup = scoreup - 1
         $('#score').html(scoreup);
         $('#fun5').show(); $("#fun4").hide();
@@ -74,47 +72,85 @@ $(document).ready(function () {
     // forth question button known as #winner 4
 
     $('#winner4').on('click', function () {
-        scoreup = scoredownr + 1
+        scoreup = scoreup + 1;
         $('#score').html(scoreup);
-        $('#endGame').show(); $('#fun5').hide();
+        $('#fun6').show(); $('#fun5').hide();
     });
 
-    $("#looser4").on('click', function () {
+    $("#looser4,#looser8").on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
         scoreup = scoreup - 1
         $('#score').html(scoreup);
-        $('#endGame').show(); $("#fun5").hide();
+        $('#fun6').show(); $("#fun5").hide();
         console.log("#looser1")
     });
-
-    // fith question button known as #endgame  
-    $('#reset').on('click', function () {
-        $('#fun1').show();
+ // this is home
+//  question five
+    $('#winner5').on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
+        scoreup = scoreup + 1
+        $('#score').html(scoreup);
+        $('#fun7').show(); $('#fun6').hide();
+        console.log(scoreup)
     });
-    $('#reset').on('click', function () {
-        $('#endGame').hide();
+    $("#looser9,#looser10").on('click', function () {
+        scoreup = scoreup - 1
+        $('#score').html(scoreup);
+        $('#fun7').show(); $("#fun6").hide();
+        console.log("Hlooser2")
     });
+    // question 6
+    $('#winner6').on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
+        scoreup = scoreup + 1
+        $('#score').html(scoreup);
+        $('#fun8').show(); $('#fun7').hide();
+        console.log(scoreup)
+     });
+    $("#looser12,#looser11").on('click', function () {
+        scoreup = scoreup - 1
+        $('#score').html(scoreup);
+        $('#fun8').show(); $("#fun7").hide();
+        console.log("Hlooser2")
+    });
+    // question 7
+    $('#winner7').on('click', function () {
+        secondsLeft = secondsLeft - 10
+        $('#time').html(secondsLeft);
+        scoreup = scoreup + 1
+        $('#score').html(scoreup);
+        $('#fun9').show(); $('#fun8').hide();
+        console.log(scoreup)
+    });
+    $("#looser13,#looser14").on('click', function () {
+        scoreup = scoreup - 1
+        $('#score').html(scoreup);
+        $('#fun9').show(); $("#fun8").hide();
+        console.log("Hlooser2")
+    });
+// question 8
+  
 
-
-    // $('#tryme').click(function(){
-    //     alert('this works');
-    // })
-
-
-    // keep this at the end of the JS/JQ
-
+  // fith question button known as #endgame  
+  $('#reset').on('click', function () {
+    $('#fun1').show();$('#endGame').hide();
+});
 
     // the timer is now put on the back burner
     // classic dont know where to start so im going to build a timer 
     var timeEl = document.querySelector("#time");
-    var mainEl = document.getElementById("main");
+    // var mainEl = document.getElementById("main");
 
-    var secondsLeft = 45;
+    var secondsLeft = 1000;
     console.log('mark')
     // a working button ya thats right
     var click = document.querySelector("#tryme");
     click.addEventListener("click", function () {
 
-        var timerInterval = setInterval(function () {
+        let timerInterval = setInterval(function () {
             secondsLeft--;
             timeEl.innerText = secondsLeft + " seconds left till ";
 
@@ -131,9 +167,7 @@ $(document).ready(function () {
 });
 click = document.querySelector("#reset");
 click.addEventListener('click', function () {
-
+ // keep this at the end of the JS/JQ
 })
 
 // a working button ya thats right
-
-``
